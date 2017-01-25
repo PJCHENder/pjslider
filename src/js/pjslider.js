49a733f1df2878a6a4d8a7fb099fdacc305cf6e0
pjchender <pjchender@gmail.com>
2017-01-25 20:15:47 +0800

    for test

--- src/js/pjslider.js
+++ src/js/pjslider.js
@@ -1,14 +1,42 @@
 ;(function($){
 
-$.prototype = function pjSlider(){
-    console.log('test');
-}
+    let plugin = {};
 
-console.log($);
+    let defaults = {
+        //  GENERAL
+        mode: 'slideShow',
+        changeSlideSpeed: 3000,
+        transitionSpeed: 400,
+        showArrow: true,
+        height: '390px',
+        caption: true,
+        autoSlide: true,
+        // preloadImages: 'visible'
+        // slideZIndex: 50,
+        wrapperClass: 'pj-wrapper'
+    };
+
+
+
+
+    $.fn.pjSlider = function(options){
+        if(this.length == 0) return this;
+
+        //  support multiple elements
+        if(this.length > 1){
+            this.each()
+        }
+
+    }
+
 
 }(jQuery));
 
+
+
+
+
 $(document).ready(function(){
-    $('.container').pjSlider;
+    $('.containers').pjSlider();
     // console.log('hello');
 })
