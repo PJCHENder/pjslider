0886b9788a0d5beb7b1f1cb48b0b28f3e538982c
pjchender <pjchender@gmail.com>
2017-01-27 23:51:47 +0800

    90% done

--- src/js/pjslider.js
+++ src/js/pjslider.js
@@ -4,16 +4,15 @@
     let plugin = {};
 
     let defaults = {
-        mode: 'slideShow',
+        mode: 'captionBottom',
         changeSlideSpeed: 3000,
         transitionSpeed: 400,
         showArrow: true,
         slideHeight: '390px',
         slideWidth: '1980px', //  CSS3 max-width property
         caption: true,
-        autoSlide: true,
-        // preloadImages: 'visible'
-        // slideZIndex: 50,
+        autoPlay: true,
+        startSlideNumber: 0,
         wrapperId: 'pj-wrapper'
     };
 
@@ -35,6 +34,7 @@
         let slider = {};
         //  set a reference to our slider element
         let el = this;
+        let wrapperId;
         //** plugin.el = this;
 
         /**
@@ -48,7 +48,6 @@
          */
 
         let init = function() {
-            let pjSliderItem = el.find('li');
             //  merge user-supplied options with the defaults
             slider.settings = $.extend({}, defaults, options);
             //  parse slideWidth setting
@@ -56,22 +55,21 @@
             //  parse slideHeight setting
             slider.settings.slideHeight = parseInt(slider.settings.slideHeight);
             //   set initial number
-            slider.settings.currentNumber = 0;
+            slider.settings.currentNumber = slider.settings.startSlideNumber;
             //  set total number of slides
-            slider.settings.sliderNumber = pjSliderItem.length;
+            slider.settings.totalSlideNumber = el.find('li').length - 1;
             //  remove arrow control if only one slide
-            slider.settings.showArrow = (slider.settings.sliderNumber === 1) ? false : true;
+            slider.settings.showArrow = (slider.settings.totalSlideNumber === 1) ? false : true;
             //  getting data-attribute value
+            let filterUndefined = sel => (!sel) ? '' : sel;
             slider.settings.data = [];
-            pjSliderItem.map(function() {
+            el.find('li').map(function() {
                 return slider.settings.data.push({
-                    photoSrc: $(this).data('photo'),
-                    title: $(this).data('title'),
-                    linkSrc: $(this).data('link')
+                    photoSrc: filterUndefined($(this).data('photo')),
+                    title: filterUndefined($(this).data('title')),
+                    linkSrc: filterUndefined($(this).data('link'))
                 });
             })
-
-
             //  perform all DOM / CSS modifications
             setup();
 
@@ -105,18 +103,19 @@
 
 
             //** Dom Modifiction
+            //  Useful Variable
+            wrapperId = $(`#${slider.settings.wrapperId}`);
             //  reset el dom
             el.html('');
             //  get jQuery Object of wrapper ID 
-            let wrapperId = $(`#${slider.settings.wrapperId}`);
             slider.settings.data.forEach(function(item, index) {
                 el.append(`
                     <li>
                         <a href='${item.linkSrc}' target='_blank'>
-                            <div class='slideItem item${index}'></div>
+                            <div class='slideItem item${index}' style='background-image:url("${item.photoSrc}")'></div>
                         </a>
                     </li>`);
-                wrapperId.find('.caption').append(`
+                wrapperId.find('.slideCaption').append(`
                     <li>${item.title}</li>
                 `);
                 wrapperId.find('.slideNav').append(`
@@ -136,13 +135,127 @@
             // init Nav CSS
             wrapperId.find('.slideNav').find('li').first().addClass('active');
 
-            let 
+            //  Get Conatiner CSS
+            slider.settings.slideWidth = slideWidth = wrapperId.width();
+            let slideHeight = slider.settings.slideHeight;
+            let slideNumber = slider.settings.totalSlideNumber;
 
+            //  Setting CSS to adjust slide
+            el.closest('.banner').css({
+                'width': slideWidth * (slideNumber + 1),
+                'height': slideHeight
+            })
+            el.find('li').css({
+                'width': slideWidth,
+                'height': slideHeight
+            })
 
+            wrapperId.find('.caption').css('width', slideWidth)
+                .find('.slideCaption').css('width', slideWidth * (slideNumber + 1))
+                .find('li').css('width', slideWidth);
 
+            //  Setting Display of Arrow control
+            if (!slider.settings.showArrow) {
+                wrapperId.find('.slideButton').remove();
+            };
+
+            //  Setting Display Mode of Slide
+            switch (slider.settings.mode) {
+                case 'captionBottom':
+                    wrapperId.find('.caption').addClass('modeCaptionBottom');
+                    break;
+
+                case 'captionCenter':
+                    wrapperId.find('.caption').addClass('modeCaptionCenter');
+                    break;
+
+                default:
+                    wrapperId.find('.caption').addClass('modeCaptionBottom');
+            }
+
+            if (slider.settings.mode === 'slide')
+
+            //  Start Slide Show
+            preLoad();
+            window.onload = start;
+        }
+
+        //  preLoad Images
+        let preLoad = function(callback) {
+            for(item of slider.settings.data){
+                new Image().src = item.photoSrc;
+            }
+        }
+
+        //  decide which slide show currently
+        let checkSlideNumber = (currentSlideNumber, totalSlideNumber) => {
+            if (currentSlideNumber < 0) {
+                return totalSlideNumber;
+            } else if (currentSlideNumber > totalSlideNumber) {
+                return 0;
+            } else {
+                return currentSlideNumber;
+            }
+        }
+
+        //  main function to make slide change
+        let changeSlide = () => {
+
+            slider.settings.currentNumber = checkSlideNumber(slider.settings.currentNumber, slider.settings.totalSlideNumber);
+            let animatePosition = -slider.settings.slideWidth * slider.settings.currentNumber;
+            //  banner animation
+            el.closest('.banner').stop(true, true).animate({
+                'left': animatePosition
+            }, slider.settings.transitionSpeed);
+
+            //  caption animation
+            wrapperId.find('.slideCaption').stop(true, true).animate({
+                'left': animatePosition
+            }, slider.settings.transitionSpeed);
+
+            //  nav control
+            wrapperId.find('.slideNav li').removeClass('active');
+            wrapperId.find('.slideNav li').eq(slider.settings.currentNumber).addClass('active');
+        }
+
+        //  autoPlay the slide
+        let autoPlay = function() {
+            if (slider.settings.autoPlay) {
+
+                let autoSlide = () => {
+                    slider.settings.currentNumber++;
+                    changeSlide();
+                }
+                let clock = setInterval(autoSlide, slider.settings.changeSlideSpeed);
+
+                // Stop while hovering
+                wrapperId.find('.slide').hover(() => clearInterval(clock), () => clock = setInterval(autoSlide, slider.settings.changeSlideSpeed));
+            }
+        }
+
+        // start all the event handler in slide
+        let start = function() {
+            //  Right Arrow Button
+            wrapperId.find('.slideButtonRight').on('click', function() {
+                slider.settings.currentNumber++;
+                changeSlide();
+            });
+            //  Left Arrow Button
+            wrapperId.find('.slideButtonLeft').on('click', function() {
+                slider.settings.currentNumber--;
+                changeSlide();
+            });
+            //  Nav Control Button
+            wrapperId.find('.slideNav li').on('click', function() {
+                slider.settings.currentNumber = $(this).data('navitem');
+                changeSlide();
+            });
+            autoPlay();
         }
 
         init();
+
+        return this;
     }
 
 
@@ -154,7 +267,8 @@
 
 $(document).ready(function() {
     $('.pjSlider').pjSlider({
-        autoSlide: false
+        changeSlideSpeed: 5000,
+        autoPlay: true,
+        mode: 'captionCenter'
     });
-    // console.log('hello');
 })
